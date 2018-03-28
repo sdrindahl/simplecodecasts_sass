326a1de9bf9a3f78428a696481b7964367dd1813
Scott Rindahl <sdrindahl@gmail.com>
2018-03-28 09:14:07 -0500

    facebook login

--- spec/features/reddit_spec.rb
+++ spec/features/reddit_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-feature 'Twitter', :post_deploy do
+feature 'Facebook', :post_deploy do
 
-  scenario 'navigate to twitter and fail to login' do
+  # scenario 'navigate to twitter and fail to login' do
     # this method comes from spec/helpers/login_spec_helper.rb  We can use it in
     # this test and in any future tests because that helper was included globally
     # in the rspec setup. To check that, take a look at the spec_helper.rb file.
@@ -15,12 +15,21 @@ feature 'Twitter', :post_deploy do
     # get use of it here because at the top of this file we say require everything
     # from spec_helper
 
-    navigate_to_twitter
-
-    fill_in 'Phone, email, or username', with: 'barf@umn.edu'
-    fill_in 'Password', with: 'krypt!k', match: :first
-    click_button 'Log in'
+    # navigate_to_twitter
+    #
+    # fill_in 'Phone, email, or username', with: 'barf@umn.edu'
+    # fill_in 'Password', with: 'krypt!k', match: :first
+    # click_button 'Log in'
 
     # expect(page).to have_text 'YOU FAILED MISERABLY TO LOG IN, TRY AGAIN!'
+  # end
+
+  scenario 'navigate to facebook login' do
+    navigate_to_facebook_login
+    fill_in 'Email or Phone', with: 'sdrindahl@gmail'
+    fill_in 'Password', with: 'MinnesotaWild!!'
+    click_button 'Log In'
+    # binding.pry
+    # click 'Messenger'
   end
 end
--- spec/helpers/login_spec_helper.rb
+++ spec/helpers/login_spec_helper.rb
@@ -4,4 +4,9 @@ module LoginSpecHelper
     visit 'https://www.twitter.com'
     expect(page).to have_link 'Forgot password?'
   end
+
+  def navigate_to_facebook_login
+    visit 'https://www.facebook.com'
+    #expect(page).to have_text 'Sign Up'
+  end
 end
